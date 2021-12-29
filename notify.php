8a2622d94d4b87d590190d1a81f28bc0b70e0a2c
kwynncom <buessresume@gmail.com>
2021-12-29 00:50:38 -0500

    refining notification process

--- notify.php
+++ notify.php
@@ -5,23 +5,42 @@ require_once('/opt/kwynn/email.php');
 require_once(__DIR__ . '/srv/serverDB.php');
 
 class notify_email_msgs extends dao_msg {
-	public function __construct() {
+
+	const nsd = 'nsd';
+
+	public function __construct() {	
 		parent::__construct();
 		$this->setup();
-		// $this->do10(); // kwynn_email::send('notify', 'testing - no limits yet');
+		$this->do10(); 
 	}
 	
 	private function setup() {
-		$this->creTabs(['n' => 'notify']);
-		
+		$this->creTabs(['n' => 'notice']);
 	}
 	
 	private function do10() { 
-		$now = $dat['ts'] = time();
-		$dat['r'] = date('r', $now);
-		$dat['sapi'] = iscli() ? 'cli' : 'web';
-		$this->ncoll->insertOne($dat);
+		$k = self::nsd;
+		$nnc = $this->mcoll->count([$k => ['$ne' => 'sent']]); 
+		if ($nnc === 0) return; // sent versus seen
+		$this->do20();
+		return;
 	}
+	
+	private function do20() {
+		$cnt = $this->ncoll->count();
+		if ($cnt > 0) return;
+		$this->do30();
+	}
+	
+	private function do30() {
+		$emr = kwynn_email::send('notify', 'testing - no limits yet');
+		$ts = $dat['ts'] = time(); $dat['r'] = date('r', $ts);
+		$dat['emstatus'] = $emr;
+		$this->ncoll->insertOne($dat);
+		$nsd = self::nsd;
+		if ($emr) $res = $this->mcoll->updateMany([$nsd => 'sweep'], ['$set' => [$nsd => 'sent']]);
+	}
+	
 }
 
 new notify_email_msgs();
